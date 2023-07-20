a51eb735740445cefcf3e0c87a0abfc9b536fa7a
ayumi24 <115326506+ayumi24@users.noreply.github.com>
2023-07-20 08:39:24 +0000

    投稿削除機能作成中

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "98246663904f4a2d97523ecf9513293f.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
+  config.web_console.whitelisted_ips = '220.97.199.149'
 end
